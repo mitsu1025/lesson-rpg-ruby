b357b3c7f942042ffe8f2ef6b571d27442361786
marotasu1010 <happyboxfavor@yahoo.co.jp>
2018-06-07 21:52:48 +0900

    error

---
+++ model/recover.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+class Recover
+   attr_reader :name, :hp
+   def initialize(name, hp)
+      @name = name
+      @hp = hp
+   end
+end
--- model/satan.rb
+++ model/satan.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 require_relative './actor'
 require_relative './magic'
+require_relative './recover'
 
 class Satan < Actor
-  attr_accessor :magic_skill
+  attr_accessor :magic_skill, :recover
 
   def attack
     return super if @magic_skill.nil?
@@ -12,6 +13,9 @@ class Satan < Actor
     if random == 4
       self.mp -= @magic_skill.mp
       { atk: @magic_skill.atk, msg: "☆☆☆☆☆#{name}は、#{@magic_skill.name}を放った！☆☆☆☆☆" }
+    elsif random == 5
+      self.hp += @recover.hp
+      { msg: "*****#{name}は、#{@recover.name}を唱えた！*****"}
     else
       super
     end
